31eb2994d76c3f05ec858091acf2005ef4e0192f
HasnatBajwa <hasnatjbajwa@gmail.com>
2023-01-24 14:49:17 +0500

    first commit

---
+++ src/Modules/Nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NavStyles.css';
+
+const Nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Nav;
---
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../../Modules/Nav/Nav';
+
+const LandingPage = () => {
+    return (
+        <>
+            <Nav />
+        </>
+    )
+}
+
+export default LandingPage
