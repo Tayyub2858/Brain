644ace45339d6f6c5e63ec224a3132773cf4e2d0
HasnatBajwa <hasnatjbajwa@gmail.com>
2023-02-09 12:28:30 +0500

    footer-updated

--- src/Modules/Footer/Footer.jsx
+++ src/Modules/Footer/Footer.jsx
@@ -14,142 +14,102 @@ const Footer = () => {
     <>
       <div className="footer-container">
         <Grid container>
-          <Grid item xs={12} sm={12} md={2}>
-            <div className="footer-objects">
-       <Link to='/'><img src={Logo} alt="" className="footer-logo" /></Link>       
-
+          <Grid item xs={12} sm={12} md={1}>
+            <div className="footer-logo-container">
+              <Link to='/'><img src={Logo} alt="Footer Logo" className='footer-logo' /></Link>
             </div>
           </Grid>
           <Grid item xs={12} sm={12} md={2}>
             <div className="address-container">
-              <h3 className="address-heading">
+              <h3 className="footer-heading">
                 HEAD OFFICE
               </h3>
-              <p className="address-txt">
-                Office 18, Street NO 9 Hassan Block,<br /> Khayaban Gardens Near Lasani Pulli <br /> Sargodah Road Faisalabad.
-              </p>
-              <p className="address-ph">
-                Phone: 041-8787246
+              <p className="footer-txt">
+                Brain Analytics, Office 18, Khayaban Gardens Sargodah Road, Faisalabad
+                <span className="ph-num">phone: 041-8787246</span>
               </p>
             </div>
           </Grid>
-          <Grid item xs={6} sm={6} md={1}>
-            <div className="footer-objects">
+          <Grid item xs={12} sm={12} md={1}>
+            <div className="footer-links">
               <h3 className="footer-heading">
                 LEARN
               </h3>
-              <p className="footer-links">
+              <p className="footer-link-txt">
                 Courses
               </p>
-              <p className="footer-links">
+              <p className="footer-link-txt">
                 Blog
               </p>
             </div>
           </Grid>
-          <Grid item xs={6} sm={6} md={1}>
-            <div className="footer-objects">
+          <Grid item xs={12} sm={12} md={1}>
+            <div className="footer-links">
               <h3 className="footer-heading">
                 RESOURCES
               </h3>
-
-              <p className="footer-links">
+              <p className="footer-link-txt">
                 Gallery
               </p>
-              <p className="footer-links">
+              <p className="footer-link-txt">
                 Help Center
               </p>
-              <p className="footer-links">
-                Site map
+              <p className="footer-link-txt">
+                Site Map
               </p>
             </div>
           </Grid>
-          <Grid item xs={6} sm={6} md={1}>
-            <div className="footer-objects">
+          <Grid item xs={12} sm={12} md={1}>
+            <div className="footer-links">
               <h3 className="footer-heading">
                 SERVICES
               </h3>
-              <p className="footer-links">
-                Corporate Training
-
-              </p>
-              <p className="footer-links">
+              <Link to='/corporate-training'>
+                <p className="footer-link-txt">
+                  Corporate Training
+                </p></Link>
+              <p className="footer-link-txt">
                 Consulting
               </p>
             </div>
           </Grid>
-          <Grid item xs={6} sm={6} md={1}>
-            <div className="footer-objects">
+          <Grid item xs={12} sm={12} md={1}>
+            <div className="footer-links">
               <h3 className="footer-heading">
                 CONNECT
               </h3>
-              <p className="footer-links">
+              <p className="footer-link-txt">
                 Meet The Team
-
               </p>
-              <p className="footer-links">
-                Contact
+              <p className="footer-link-txt">
+                Connect
               </p>
-
             </div>
           </Grid>
           <Grid item xs={12} sm={12} md={2}>
-          <h3 className="footer-heading">
-               NEWS LETTER
+            <div className="newsletter-container">
+              <h3 className="footer-heading">
+                NEWS LETTER
               </h3>
-            <p className="footer-txt-newsletter">
-                Subscribe to our news letter for more updates
+              <p className="footer-txt">
+                Subscribe our News Letter for updates
               </p>
-              <input type="text" placeholder='barry@hbo.com' className="sub" />
-              <button className="subs-btn">SUBSCRIBE</button>
-              
-          </Grid>
-          <Grid className='link-container' item xs={12} sm={12} md={1}>
-  
-            <img src={barcode} alt="barcode" className="barcode" />
+              <input type="text" placeholder='info@brainanalytics.pk' className="sub" />
+              <button className="sub-btn"> Subscribe</button>
+            </div>
           </Grid>
           <Grid item xs={12} sm={12} md={1}>
-            <Link
-              to="https://www.facebook.com/BrainAnalytics.pk"
-              target="_blank"
-              rel="noopener noreferrer">
-              <img src={fb} alt="barcode" className="social-media-icons" />
-            </Link>
-            <Link
-                  to="https://www.linkedin.com/company/brainanalytics/"
-                  target="_blank"
-                  rel="noopener noreferrer">
-              <img src={li} alt="barcode" className="social-media-icons" />
-                 
-                </Link>
-                <Link
-                  to="https://www.facebook.com/BrainAnalytics.pk"
-                  target="_blank"
-                  rel="noopener noreferrer">
-              <img src={tw} alt="barcode" className="social-media-icons" />
-            </Link>
-            <Link
-                  to="https://www.facebook.com/BrainAnalytics.pk"
-                  target="_blank"
-                  rel="noopener noreferrer">
-              <img src={insta} alt="barcode" className="social-media-icons" />
-                 </Link>
+            <div className="barcode">
+              <img src={barcode} alt="barcode" className="barcode-img" />
+            </div>
           </Grid>
-        </Grid>
-      </div>
-      <div className="end-footer-container">
-        <Grid container>
-          <Grid item xs={12} sm={12} md={6}>
-            <p className="endfooter-txt">
-              &copy; Brain Analytics, LLC | All Rights Reserved
-            </p>
-          </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-            <p className="endfooter-links">
-
-              <span className="endfooter-link">Terms&Conditions</span>
-              <span className="endfooter-link">Privacy</span>
-              <span className="endfooter-link">Contact</span>
-            </p>
+          <Grid item xs={12} sm={12} md={1}>
+            <div className="socialmedia-links">
+            <img src={fb} alt="facebook" className="social-media-img" />
+            <img src={insta} alt="instagram" className="social-media-img" />
+            <img src={li} alt='linked in' className="social-media-img" />
+            <img src={tw} alt="twitter" className="social-media-img" />
+            </div>
           </Grid>
         </Grid>
       </div>
@@ -157,42 +117,3 @@ const Footer = () => {
   )
 }
 export default Footer;
-{/* <Grid item xs={12} sm={12} md={1}>
-          <span>
-                <svg width="0" height="0">
-                  <linearGradient id="blue-gradient" x1="100%" y1="100%" x2="0%" y2="0%">
-                    <stop stopColor="#25257c" offset="0%" />
-                    <stop stopColor="#FACA28" offset="100%" />
-                  </linearGradient>
-                </svg>
-                
-                <Link
-                  to="https://www.facebook.com/BrainAnalytics.pk"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <AiFillInstagram
-                    style={{ fill: "url(#blue-gradient)" }}
-                    className='social-media-links' />
-                </Link>
-                
-                <Link
-                  to="https://www.facebook.com/BrainAnalytics.pk"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  <AiFillTwitterSquare
-                    style={{ fill: "url(#blue-gradient)" }}
-                    className='social-media-links' />
-                </Link>
-              </span>
-          </Grid> */}
-{/* <p className="footer-txt">
-                Empowering everyday people with life-changing data skills.
-              </p> */}
-{/* <p className="footer-txt-newsletter">
-                Subscribe to the newsletter
-              </p>
-              <input type="text" placeholder='barry@hbo.com' className="sub" />
-              <button className="subs-btn">SUBSCRIBE</button>
-              <p className="footer-txt">
-                <i>No spam, just helpful tips & tricks delivered directly to your inbox.</i>
-              </p> */}
