a5fde0231ede63b88e5a4761dc2446314ba0aa6a
HasnatBajwa <hasnatjbajwa@gmail.com>
2023-02-20 17:26:49 +0500

    updated

--- src/Modules/Module 4/Career.jsx
+++ src/Modules/Module 4/Career.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import './CareerStyle.css';
-import saqibriaz from '../../Assets/Front-Page/saqibriaz.png'
+import saqibriaz from '../../Assets/Front-Page/Saqib-Riaz.png'
+import shokatjaved from '../../Assets/Front-Page/Shoukat.png'
+import zafarabbas from '../../Assets/Front-Page/Zafar-Abbas.png'
+import hafiztayyab from '../../Assets/Front-Page/Hafiz-Tayyab.png'
+import irfan from '../../Assets/Front-Page/Irfan.png'
+
 import { Grid } from "@material-ui/core";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper";
@@ -33,7 +38,9 @@ const Career = () => {
                                 </Grid>
                                 <Grid item xs={12} sm={12} md={3}>
                                     <img src={saqibriaz} alt="" className="caree-img" />
-                                    <h3 className="career-img-name">Saqib Riaz</h3>
+                                    <h3 className="career-img-name">Mr.Saqib Riaz, FCA</h3>
+                                    <h4 className="career-img-name">Cheif Financial Officer, <br /> Etihad Power Generation Limited</h4>
+
                                 </Grid>
                             </Grid>
 
@@ -47,8 +54,10 @@ const Career = () => {
                                         These courses gave me mind-blowing articulation of how to become a great IT professional. Thank you!”</p>
                                 </Grid>
                                 <Grid item xs={12} sm={12} md={3}>
-                                    <img src={saqibriaz} alt="" className="caree-img" />
-                                    <h3 className="career-img-name">Saqib Riaz</h3>
+                                    <img src={shokatjaved} alt="" className="caree-img" />
+                                    <h3 className="career-img-name">Mr.Shoukat Javed</h3>
+                                    <h4 className="career-img-name">Deputy Manager IT, <br /> J.K Spinning Mills</h4>
+
                                 </Grid>
                             </Grid>
 
@@ -62,8 +71,41 @@ const Career = () => {
                                         These courses gave me mind-blowing articulation of how to become a great IT professional. Thank you!”</p>
                                 </Grid>
                                 <Grid item xs={12} sm={12} md={3}>
-                                    <img src={saqibriaz} alt="" className="caree-img" />
-                                    <h3 className="career-img-name">Saqib Riaz</h3>
+                                    <img src={zafarabbas} alt="" className="caree-img" />
+                                    <h3 className="career-img-name">Mr. Zafar Abbas</h3>
+                                    <h4 className="career-img-name">Regional Sales Manager,<br /> Pharmasol (PVT.) LTD.</h4>
+                                </Grid>
+                            </Grid>
+
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="career-box">
+                            <Grid container>
+                                <Grid item xs={12} sm={12} md={9}>
+                                    <p className="career-text">“I feel lucky and grateful to have found Brain Analytics in the early stage of my career.
+                                        These courses gave me mind-blowing articulation of how to become a great IT professional. Thank you!”</p>
+                                </Grid>
+                                <Grid item xs={12} sm={12} md={3}>
+                                    <img src={hafiztayyab} alt="" className="caree-img" />
+                                    <h3 className="career-img-name">Mr. Hafiz Tayyab</h3>
+                                    <h4 className="career-img-name">Cheif Financial Officer, <br /> United Empire Properties LLP</h4>
+                                </Grid>
+                            </Grid>
+
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="career-box">
+                            <Grid container>
+                                <Grid item xs={12} sm={12} md={9}>
+                                    <p className="career-text">“I feel lucky and grateful to have found Brain Analytics in the early stage of my career.
+                                        These courses gave me mind-blowing articulation of how to become a great IT professional. Thank you!”</p>
+                                </Grid>
+                                <Grid item xs={12} sm={12} md={3}>
+                                    <img src={irfan} alt="" className="caree-img" />
+                                    <h3 className="career-img-name">Mr. Irfan Younas</h3>
+                                    <h4 className="career-img-name">Group Head Of Internal Audit, <br /> Etihad Power Generation Limited</h4>
                                 </Grid>
                             </Grid>
 
--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import Nav from "../../Modules/Nav/Nav";
 import DataAnalysic from "../../Modules/Module 1/DataAnalysic";
 import Footer from "../../Modules/Footer/Footer";
-import Companies from "../../Modules/Module15/Companies";
+import Companies from "../../Modules/Module 10/Companies";
 import Software from "../../Modules/Module 2/Software";
 import DreamJob from "../../Modules/Module 3/DreamJob";
-import TeamTraining from "../../Modules/Module14/TeamTraining";
-import Connect from "../../Modules/Module13/Connect";
-import Showcase from "../../Modules/Module11/Showcase";
+import TeamTraining from "../../Modules/Module 9/TeamTraining";
+import Connect from "../../Modules/Module 8/Connect";
+import Showcase from "../../Modules/Module 7/Showcase";
 import Career from "../../Modules/Module 4/Career";
-import Build from "../../Modules/Module 6/Build";
-import Skills from "../../Modules/Module 7/Skills";
+import Build from "../../Modules/Module 5/Build";
+import Skills from "../../Modules/Module 6/Skills";
 const LandingPage = () => {
   return (
     <>
